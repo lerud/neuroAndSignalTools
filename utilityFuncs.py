3ea37472bbfeaf0c170c6bf799e4c52665b908c3
Karl Lerud <karl.lerud@gmail.com>
2025-10-14 23:30:57 -0400

    Use triple quotes to escape weird chars in filename for rsync calls

--- utilityFuncs.py
+++ utilityFuncs.py
@@ -7,5 +7,5 @@ def doRsync(inputFile):
     os.chdir(sourceHome)
     inputFile = inputFile.replace(sourceHome, "")
     print(inputFile)
-    subprocess.run("rsync -avhR --progress " + inputFile + " /Users/karl/map", shell=True)
+    subprocess.run('''rsync -avhR --progress "''' + inputFile + '''" /Users/karl/map''', shell=True)
     return "/Users/karl/map/" + inputFile
